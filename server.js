6808ee21cbac342df8eb06b9cc2c89f6cc103c47
Pasham Sathish Kumar <98509690+sathishpasham@users.noreply.github.com>
2024-03-07 15:58:23 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+require('dotenv').config()
+const dbConfig=require('./config/dbConfig')
+const userRoute=require('./Routes/userRoute')
+app.use(express.json())
+app.use('/',userRoute)
+
+
+
+
+
+
+
+app.listen(8097,()=>{
+    console.log('server started!')
+})
